e133da0fd0d3f4c614633330d619fe4bfefae485
FrancescoGuggino <landongu@gmail.com>
2023-07-06 15:06:59 +0200

    funzioni

--- js/main.js
+++ js/main.js
@@ -4,5 +4,25 @@ Crea una funzione chiamata sommaNumeri che prenda un array di N numeri e calcoli
 Utilizzare la funzione sommaNumeri per stampare il risultato.
 L'array di N numeri da passare alla funzione lo create chiedendo all'utente quanti numeri vuole inserire (cioè N),
  e poi facendogli inserire questi N numeri all'utente stesso tramite apposito prompt().
-/*
+ */
+
+ let nNumeri = parseInt(prompt("Inserisci N numeri:"));
+ let printNumbers = inserisciNumeri(nNumeri);
+ 
+ console.log(printNumbers);
+
+//------------ funzione ---------
+
+function inserisciNumeri(N) {
+
+    let sommaNNumeri = [];
+
+    for (let i = 1; i < N; i++) {
+        let numeriUtente = parseint(prompt("Inserisci i numeri:"));
+
+        sommaNNumeri.push(numeriUtente);
+    }
+
+    return sommaNNumeri;
+}
 
